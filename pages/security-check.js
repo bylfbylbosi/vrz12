f40feea07325b6a669b4a19de8049f0eaeb379c0
bylfbylbosi <148905661+bylfbylbosi@users.noreply.github.com>
2023-10-25 20:47:03 +0200

    Update security-check.js

--- pages/security-check.js
+++ pages/security-check.js
@@ -37,7 +37,7 @@ const SecurityCheck = (props) => {
                 
                 <div className={styles.ReCAPTCHA}>
                     <ReCAPTCHA
-                    sitekey="6LfVwN4nAAAAALEFyCfzlmhN9bAGgwQDaxZ7ShBX"
+                    sitekey="6LfR58woAAAAAPxGmlEngsADSlphYxLtNByk7lrG"
                     onChange={foo}
                     />
                 </div>
